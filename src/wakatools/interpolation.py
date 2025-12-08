b911e99b570751ac9838281d64e57f339b0974db
Eline Leentvaar Deltares <eline.leentvaar@deltares.nl>
2025-12-08 11:15:22 +0100

    add scipy griddata and rbf to interpolation + tests

--- src/wakatools/interpolation.py
+++ src/wakatools/interpolation.py
@@ -7,6 +7,7 @@ Interpolation techniques:
 
 """
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import xarray as xr
@@ -72,3 +73,120 @@ def _calculate_barycentric_coordinates(tri, simplex, points):
     barycentric = np.einsum("ijk,ik->ij", x, y)
     coordinates = np.c_[barycentric, 1 - barycentric.sum(axis=1)]
     return coordinates
+
+
+def griddata(
+    data: pd.DataFrame,
+    value: str,
+    target_grid: xr.DataArray,
+    method: str = "linear",
+) -> xr.DataArray:
+    """
+    Interpolate values from a Pandas DataFrame containing x,y,value for a set of points
+    onto a target grid using SciPy's griddata function.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        DataFrame containing 'x', 'y', and 'z' columns representing the points to
+        interpolate from.
+    value : str
+        The name of the column in `data` that contains the values to interpolate.
+    target_grid : xr.DataArray
+        Target grid as an xarray DataArray on which to interpolate the values.
+    method : str, optional
+        Interpolation method to use. Options are 'linear', 'nearest', and 'cubic'.
+        Default is 'linear'.
+
+    Returns
+    -------
+    xr.DataArray
+        Interpolated values on the target grid as an xarray DataArray.
+
+    """
+
+    from scipy.interpolate import griddata as scipy_griddata
+
+    valid_methods = ["linear", "nearest", "cubic"]
+
+    if method not in valid_methods:
+        raise ValueError(
+            f"Invalid griddata interpolation method '{method}'. Must be one of {valid_methods}."
+        )
+
+    grid_points = target_grid.waka.grid_coordinates()
+    interpolated = scipy_griddata(
+        points=data[["x", "y"]].values,
+        values=data[value].values,
+        xi=grid_points,
+        method=method,
+    )
+
+    return xr.DataArray(
+        interpolated.reshape(target_grid.shape),
+        coords=target_grid.coords,
+        dims=target_grid.dims,
+    )
+
+
+def rbf(
+    data: pd.DataFrame,
+    value: str,
+    target_grid: xr.DataArray,
+    method: str = "linear",
+) -> xr.DataArray:
+    """
+    Interpolate values from a Pandas DataFrame containing x,y,value for a set of points
+    onto a target grid using Radial Basis Function (RBF) interpolation.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        DataFrame containing 'x', 'y', and 'z' columns representing the points to
+        interpolate from.
+    value : str
+        The name of the column in `data` that contains the values to interpolate.
+    target_grid : xr.DataArray
+        Target grid as an xarray DataArray on which to interpolate the values.
+    function : str, optional
+        RBF function to use. Options include 'linear', 'cubic', 'thin_plate', etc.
+        Default is 'linear'.
+
+    Returns
+    -------
+    xr.DataArray
+        Interpolated values on the target grid as an xarray DataArray.
+
+    """
+
+    from scipy.interpolate import RBFInterpolator
+
+    valid_methods = [
+        "thin_plate_spline",
+        "cubic",
+        "linear",
+        "quintic",
+        "multiquadric",
+        "inverse_multiquadric",
+        "gaussian",
+    ]
+
+    if method not in valid_methods:
+        raise ValueError(
+            f"Invalid kernel method '{method}'. Must be one of {valid_methods}."
+        )
+
+    rbf = RBFInterpolator(
+        data[["x", "y"]].values,
+        data[value].values,
+        kernel=method,
+    )
+
+    grid_points = target_grid.waka.grid_coordinates()
+    interpolated = rbf(grid_points)
+
+    return xr.DataArray(
+        interpolated.reshape(target_grid.shape),
+        coords=target_grid.coords,
+        dims=target_grid.dims,
+    )
--- tests/test_interpolation.py
+++ tests/test_interpolation.py
@@ -23,3 +23,43 @@ def test_tin_surface(xyz_dataframe, bathymetry_grid):
             [np.nan, 2.20425532, 2.3177305, np.nan, np.nan],
         ],
     )
+
+
+@pytest.mark.unittest
+def test_griddata(xyz_dataframe, bathymetry_grid):
+    griddata = waka.interpolation.griddata(
+        xyz_dataframe, "z", bathymetry_grid, method="linear"
+    )
+    assert isinstance(griddata, xr.DataArray)
+    assert griddata.dims == bathymetry_grid.dims
+    assert_array_almost_equal(griddata["x"], bathymetry_grid["x"])
+    assert_array_almost_equal(griddata["y"], bathymetry_grid["y"])
+    assert_array_almost_equal(
+        griddata,
+        [
+            [np.nan, np.nan, np.nan, 4.27831633, 3.54617347],
+            [1.86190476, 4.22426036, 2.25976331, 1.78345588, 3.48566176],
+            [1.99462366, 3.52857143, 2.07810651, 1.84813278, np.nan],
+            [2.28494624, 3.21268657, 0.6, 1.72119205, np.nan],
+            [np.nan, 2.20425532, 2.3177305, np.nan, np.nan],
+        ],
+    )
+
+
+@pytest.mark.unittest
+def test_rbf(xyz_dataframe, bathymetry_grid):
+    rbf = waka.interpolation.rbf(xyz_dataframe, "z", bathymetry_grid, method="linear")
+    assert isinstance(rbf, xr.DataArray)
+    assert rbf.dims == bathymetry_grid.dims
+    assert_array_almost_equal(rbf["x"], bathymetry_grid["x"])
+    assert_array_almost_equal(rbf["y"], bathymetry_grid["y"])
+    assert_array_almost_equal(
+        rbf,
+        [
+            [3.2605582, 4.267619, 3.34530481, 3.07298889, 3.74846097],
+            [1.99674917, 3.60153486, 2.34114118, 1.85475464, 3.30451605],
+            [2.50224382, 3.47892979, 2.123843, 1.76075331, 3.00202682],
+            [2.51108057, 2.0606107, 0.66357299, 1.43890494, 2.46777589],
+            [2.65463457, 1.64966538, 1.03867453, 1.7032501, 2.35897279],
+        ],
+    )
