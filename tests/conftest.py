f31f4cec537e2f276c2932763e9dcf13449e9060
Eline Leentvaar Deltares <eline.leentvaar@deltares.nl>
2025-12-18 10:10:57 +0100

    add single horizon seismic data file in testdata and fixture in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -133,28 +133,6 @@ def sample_grid():
 
 
 @pytest.fixture
-def single_horizon_seismic_file(tmp_path: Path):
-    content = (
-        "182043.95333,334581.60333,BwxTrajec_10_20240709_125934_P61_T1_RAW_LF,3629.00, 0.0064,88.8421\n"
-        "182043.89000,334581.39000,BwxTrajec_10_20240709_125934_P61_T1_RAW_LF,3630.00, 0.0064,1168.08\n"
-        "182043.84000,334581.22000,BwxTrajec_10_20240709_125934_P61_T1_RAW_LF,3631.00, 0.0064,-643.247\n"
-        "182043.77667,334581.00667,BwxTrajec_10_20240709_125934_P61_T1_RAW_LF,3632.00, 0.0064,-823.187\n"
-        "182043.71333,334580.79333,BwxTrajec_10_20240709_125934_P61_T1_RAW_LF,3633.00, 0.0064,-643.717\n"
-        "182043.65000,334580.58000,BwxTrajec_10_20240709_125934_P61_T1_RAW_LF,3634.00, 0.0064,-1149.45\n"
-        "182043.60000,334580.40000,BwxTrajec_10_20240709_125934_P61_T1_RAW_LF,3635.00, 0.0064,-1357.83\n"
-        "182387.29000,335337.84000,BwxTrajec_10_P_-1_20240709_101637_P61_T1_RAW_LF,2.00, 0.0046,-1009.27\n"
-        "182387.20000,335337.60000,BwxTrajec_10_P_-1_20240709_101637_P61_T1_RAW_LF,3.00, 0.0046,-804.448\n"
-        "182387.11000,335337.36000,BwxTrajec_10_P_-1_20240709_101637_P61_T1_RAW_LF,4.00, 0.0046,-1521\n"
-        "182387.02500,335337.14500,BwxTrajec_10_P_-1_20240709_101637_P61_T1_RAW_LF,5.00, 0.0046,-3237.2\n"
-        "182386.94000,335336.93000,BwxTrajec_10_P_-1_20240709_101637_P61_T1_RAW_LF,6.00, 0.0046,-4206.23\n"
-        "182386.87000,335336.76000,BwxTrajec_10_P_-1_20240709_101637_P61_T1_RAW_LF,7.00, 0.0046,-4236.83\n"
-    )
-    filepath = tmp_path / "seismic_single_horizon.dat"
-    filepath.write_text(content)
-    return filepath
-
-
-@pytest.fixture
 def testdatadir():
     return Path(__file__).parent / "testdata"
 
@@ -162,3 +140,8 @@ def testdatadir():
 @pytest.fixture
 def seismic_multi_horizon_file(testdatadir: Path):
     return testdatadir / "multi_horizon_seismic.dat"
+
+
+@pytest.fixture
+def seismic_single_horizon_file(testdatadir: Path):
+    return testdatadir / "single_horizon_seismic.dat"
