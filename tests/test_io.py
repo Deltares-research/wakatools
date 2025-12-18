3fec3e200a6eaea7885f20d8c77c2e31fabb31b6
Eline Leentvaar Deltares <eline.leentvaar@deltares.nl>
2025-12-18 14:05:29 +0100

    fix discovery error: init changes to io.read

--- tests/test_io.py
+++ tests/test_io.py
@@ -3,7 +3,7 @@ import re
 import pandas as pd
 import pytest
 
-from wakatools.io import read
+from wakatools.io import kingdom_exports, read
 
 
 @pytest.mark.unittest
@@ -14,3 +14,9 @@ def test_read_seismics(single_horizon_seismic_file):
 @pytest.mark.unittest
 def test_read_multi_horizon_seismic(seismic_multi_horizon_file):
     read.read_multi_horizon_seismic(seismic_multi_horizon_file)
+
+
+@pytest.mark.unittest
+def test_geocard7(seismic_multi_horizon_file):
+    df = kingdom_exports.geocard7(seismic_multi_horizon_file)
+    assert isinstance(df, pd.DataFrame)
