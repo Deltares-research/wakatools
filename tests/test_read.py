520790efe8664cb830e40c445ba189f83088e55f
smknaake <bas.knaake@deltares.nl>
2025-12-22 10:07:32 +0100

    Track 'features/waka' branch from GeoST for co-development; recover lost unittest in merge-conflict

--- tests/test_read.py
+++ tests/test_read.py
@@ -1,6 +1,4 @@
-import re
-
-import pandas as pd
+import geost
 import pytest
 
 from wakatools.io import read
@@ -9,3 +7,14 @@ from wakatools.io import read
 @pytest.mark.unittest
 def test_read_seismics(seismic_single_horizon_file):
     read.read_seismics(seismic_single_horizon_file)
+
+
+@pytest.mark.unittest
+def test_read_borehole_xml(testdatadir):
+    files = testdatadir.glob("87078_HB*.xml")
+    cores = read.read_borehole_xml(files, type_="geotechnical", company="Wiertsema")
+    assert isinstance(cores, geost.BoreholeCollection)
+    assert len(cores) == 2
+
+    with pytest.raises(ValueError, match="Unsupported borehole type: unsupported_type"):
+        read.read_borehole_xml(files, type_="unsupported_type")
